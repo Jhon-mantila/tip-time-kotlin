90468dec16e5074ee4c4d1a00d3ac0e1c735a073
Jhon-mantila <kaiser_4tu@hotmail.com>
2024-04-25 21:35:05 -0500

    Pruebas locales

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -194,9 +195,11 @@ fun roundTheTipRow(
     }
 }
 
-private fun calculateTip(
+@VisibleForTesting
+internal fun calculateTip(
     amount: Double,
-    tipPercent: Double = 15.0, roundUp: Boolean
+    tipPercent: Double = 15.0,
+    roundUp: Boolean
 ): String{
 
     var tip = tipPercent / 100 * amount
