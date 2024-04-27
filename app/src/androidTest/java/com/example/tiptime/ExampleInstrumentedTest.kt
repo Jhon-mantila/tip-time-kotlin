47889914ea0d0d25e7151de31a2460189ca3f154
Jhon-mantila <kaiser_4tu@hotmail.com>
2024-04-27 12:32:15 -0500

    Aggregar pruebas de instrumentadas

--- app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
@@ -1,12 +1,21 @@
 package com.example.tiptime
 
+import android.view.Surface
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.tiptime.ui.theme.TipTimeTheme
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
+import java.text.NumberFormat
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +24,28 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
     @Test
     fun useAppContext() {
+
+        composeTestRule.setContent {
+            TipTimeTheme {
+
+                    TipTimeLayout()
+
+            }
+        }
+
+        composeTestRule.onNodeWithText("Bill Amount").performTextInput("10")
+        composeTestRule.onNodeWithText("Tip Percentage").performTextInput("20")
+
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists("No node with this test was found")
+
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.tiptime", appContext.packageName)
+        //val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        //assertEquals("com.example.tiptime", appContext.packageName)
     }
 }
